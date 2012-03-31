f9b1c4f8d08753a1536af773a14d9cc1b9d5b1db
Lars Hesel Christensen <larshesel@gmail.com>
2012-03-31 23:28:56 +0200

    changed after codereview. Very educational.

--- src/mews_webserver.erl
+++ src/mews_webserver.erl
@@ -3,9 +3,8 @@
 
 -define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
-start(SupCFG) -> 
-    [{ip, _Ip},
-     {port, Port}] = SupCFG,
+start(Cfg) -> 
+    Port = proplists:get_value(port, Cfg),
     listen(Port).
     
 
@@ -18,8 +17,6 @@ do_accept(Lsocket, N) ->
     spawn(fun() -> send_page(Socket) end), 
     error_logger:info_msg("Spawned socket number: ~p~n", [N]),
     do_accept(Lsocket, N + 1).
-
-%%<<"GET / HTTP/1.1\r\nHost: localhost:8007\r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.83 Safari/535.11\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: gzip,deflate,sdch\r\nAccept-Language: en-US,en;q=0.8\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\r\n\r\n">><<"GET /favicon.ico HTTP/1.1\r\nHost: localhost:8007\r\nConnection: keep-alive\r\nAccept: */*\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.83 Safari/535.11\r\nAccept-Encoding: gzip,deflate,sdch\r\nAccept-Language: en-US,en;q=0.8\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\r\n\r\n">>
 
 send_page(Socket) ->
     %% TODO: need to make sure that we have read the entire request... 
@@ -35,7 +32,7 @@ send_page(Socket) ->
     end.
 
 handle_request(Socket, ParsedRequest) -> 
-    {Request, Data} = ParsedRequest,
+    {Request, _Data} = ParsedRequest,
     error_logger:info_msg("handle_request: Request: ~p~n:", [ParsedRequest]),
     case Request of 
 	get ->
@@ -48,19 +45,8 @@ handle_request(Socket, ParsedRequest) ->
 parse_request([RequestLine | _Tail]) ->
     error_logger:info_msg("Parse requestLine: ~p~n", [RequestLine]),
     %% parse the first line - that's enough for now.
-    case parse_request_line(string:tokens(RequestLine, " ")) of 
-    	{ok, Request} ->
-    	    {ok, Request};
-    	error ->
-    	    {error, [parser_error, RequestLine]}
-    end.
-
-parse_request_line([FirstToken | Tail]) ->
-    error_logger:info_msg("FirstToken: ~p~n", [FirstToken]),
-    case FirstToken of 
-	"GET" ->
-	    [Uri, Version] = Tail,
-	    {ok, {get, {{uri, Uri}, {version, Version}}}};
+    case string:tokens(RequestLine, " ") of 
+	["GET", Uri, Version] -> {ok, {get, [{uri, Uri}, {version, Version}]}};
 	_ -> error
     end.
 
@@ -75,7 +61,7 @@ build_header(Data) ->
      get_server(), 
      "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\n", 
      "Accept-Ranges: bytes\n",
-     "Content-Length: ", integer_to_list(lists:flatlength(Data)), "\n", 
+     "Content-Length: ", integer_to_list(iolist_size(Data)), "\n", 
      "Connection: close\n", 
      "Content-Type: text/html; charset=UTF-8\n\n",
      Data].
@@ -83,3 +69,5 @@ build_header(Data) ->
 get_server() ->
     "Server: MyErlangWebserver. Erlang-home-made-and-backed 0.01\n".
 
+%% keep this around for inspirational purposes ;)
+%%<<"GET / HTTP/1.1\r\nHost: localhost:8007\r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.83 Safari/535.11\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: gzip,deflate,sdch\r\nAccept-Language: en-US,en;q=0.8\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\r\n\r\n">><<"GET /favicon.ico HTTP/1.1\r\nHost: localhost:8007\r\nConnection: keep-alive\r\nAccept: */*\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.83 Safari/535.11\r\nAccept-Encoding: gzip,deflate,sdch\r\nAccept-Language: en-US,en;q=0.8\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\r\n\r\n">>
