0bef90ff2175ec1bed289a19e85ae79fb072a03a
Lars Hesel Christensen <larshesel@gmail.com>
2012-04-01 17:41:31 +0200

    added my first two unit tests

--- test/http_requests_tests.erl
+++ test/http_requests_tests.erl
@@ -1,6 +1,10 @@
 -module(http_requests_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+get_bad_request_test() ->
+    {bad_request, _ } = mews_parse_request:parse_request(["blahblah"]).
 
-first_test() ->
-    42.
+%% this will be invalid, when we start checking if Uri and Version is supported
+accept_get_request_test() ->
+    {ok, _} = mews_parse_request:parse_request(["GET / HTTP/1.1"]).
+    
