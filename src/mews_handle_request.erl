34ac14b2fd7b5ed5a643324fdf06df7f7c0fa278
Lars Hesel Christensen <larshesel@gmail.com>
2012-04-12 21:46:30 +0200

    serve a static page

--- src/mews_handle_request.erl
+++ src/mews_handle_request.erl
@@ -2,19 +2,20 @@
 -export([handle_request/2]).
 
 handle_request(Socket, ParsedRequest) -> 
-    {Request, _Data} = ParsedRequest,
+    {Request, Data} = ParsedRequest,
     error_logger:info_msg("handle_request: Request: ~p~n:", [ParsedRequest]),
     case Request of 
 	get ->
-	    handle_get_request(Socket, 0);
+	    handle_get_request(Socket, Data);
 	_ ->
 	    error_logger:warning_msg("Request not supported: ~p~n:", [ParsedRequest])
     end.
 
-
-handle_get_request(Socket, N) ->	
-    Page=build_header(["Hello World: ", integer_to_list(N)]),
-    gen_tcp:send(Socket, Page),
+handle_get_request(Socket, Data) ->
+    Uri = proplists:get_value(uri, Data),
+    error_logger:info_msg("handle_get_request: reading file: ~p~n", [Uri]),
+    {ok, Binary} = file:read_file("./test_data/webroot/index.html"),
+    gen_tcp:send(Socket, build_header(Binary)),
     gen_tcp:close(Socket).    
 
 build_header(Data) -> 
@@ -25,7 +26,7 @@ build_header(Data) ->
      "Accept-Ranges: bytes\n",
      "Content-Length: ", integer_to_list(iolist_size(Data)), "\n", 
      "Connection: close\n", 
-     "Content-Type: text/html; charset=UTF-8\n\n",
+     "Content-Type: text/html; charset=UTF-8\n\n", 
      Data].
     
 get_server() ->
