53ec268c1372329b0b3190d15b234ebeaa9925f2
Lars Hesel Christensen <larshesel@gmail.com>
2012-04-15 22:36:39 +0200

    changed mews_webserver to a gen_server. So far only supports init and change of listening TCP port

--- src/mews_webserver.erl
+++ src/mews_webserver.erl
@@ -1,22 +1,194 @@
+%%%-------------------------------------------------------------------
+%%% @author Lars Hesel Christensen <>
+%%% @copyright (C) 2012, Lars Hesel Christensen
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15 Apr 2012 by Lars Hesel Christensen <>
+%%%-------------------------------------------------------------------
 -module(mews_webserver).
--export([listen/1, start/1]).
+
+
+-behaviour(gen_server).
+
+%% API
+-export([start/1, stop/0]).
+-export([change_port/1, stop_accepting_requests/0, start_accepting_requests/0]).
+%%-export([listen/1, start/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE). 
+
+-record(state, {cfg, listen_socket}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+start(Cfg) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, Cfg, []).
+
+stop() ->
+    gen_server:call(?MODULE, stop).
+
+change_port(Port) ->
+    gen_server:call(?MODULE, {change_port, Port}).
+
+stop_accepting_requests() ->
+    gen_server:call(?MODULE, {stop_accepting_requests}).
+
+start_accepting_requests() ->
+    gen_server:call(?MODULE, {start_accepting_requests}).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+%% start_link() ->
+%%     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init(Cfg) ->
+    error_logger:info_msg("gen_server:init, Cfg = ~p~n", [Cfg]),
+    ListenSocket = listen(proplists:get_value(port, Cfg)),
+    {ok, #state{cfg=Cfg, listen_socket=ListenSocket}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call({change_port, NewPort}, _From, OldState) ->
+    %% close the old listening socket
+    gen_tcp:close(OldState#state.listen_socket),
+
+    error_logger:info_msg("OldConfig: ~p~n", [OldState]),
+
+    %% Update the configuration
+    NewCfg = lists:map(fun({Name, Value}) -> 
+				case Name of 
+				    port ->
+					{port, NewPort};
+				    _ -> {Name, Value}
+				end
+			end,
+			OldState#state.cfg),
+    error_logger:info_msg("NewConfig: ~p~n", [NewCfg]),
+
+    %% start to listen on the new port.
+    listen(proplists:get_value(port, NewCfg)),
+
+    Reply = ok,
+    {reply, Reply, OldState#state{cfg=NewCfg}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+
 
 -define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
-start(Cfg) -> 
-    Port = proplists:get_value(port, Cfg),
-    listen(Port).
-    
-
 listen(Port) ->
-    {ok, LSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
-    do_accept(LSocket, 0).
+    case gen_tcp:listen(Port, ?TCP_OPTIONS) of 
+	{ok, LSocket} -> 
+	    spawn(fun() ->
+			  do_accept(LSocket, 0)
+		  end),
+	    LSocket;
+	{error, Reason} -> {error, Reason}
+    end.
 
 do_accept(Lsocket, N) ->
-    {ok, Socket} = gen_tcp:accept(Lsocket),
-    spawn(fun() -> send_page(Socket) end), 
-    error_logger:info_msg("Spawned socket number: ~p~n", [N]),
-    do_accept(Lsocket, N + 1).
+    case gen_tcp:accept(Lsocket) of 
+	{ok, Socket} ->
+	    spawn(fun() -> send_page(Socket) end), 
+	    error_logger:info_msg("Spawned socket number: ~p~n", [N]),
+	    do_accept(Lsocket, N + 1);
+	{error, closed} -> 
+	    %% the listening socket was closed, just end the process.
+	    ok
+    end.
 
 send_page(Socket) ->
     %% TODO: need to make sure that we have read the entire request... 
